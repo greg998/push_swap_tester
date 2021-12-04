23588baef5e8751793faee03af6c79af35763508
Gregory GIQUIAUD <ggiquiau@e3r10p23.clusters.42paris.fr>
2021-12-04 14:20:55 +0100

    valgrind test

---
+++ srcs/random.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+static inline int in(int *nums, int len, int val)
+{
+    int i = 0;
+    while (i < len)
+    {
+        if (val == nums[i])
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    int quantity;
+    long range;
+    int i = 0, randomNum;
+
+    quantity = atoi(argv[1]);
+    int nums[quantity];
+    range = 2147483647;
+    if (argc == 3)
+        range = atoi(argv[2]);
+    range++;
+    srand(time(NULL) + getpid());
+    while (i < quantity)
+    {
+        randomNum = rand() % range;
+        if (rand() % 2 == 1)
+            randomNum = -randomNum - 1;
+        while (in(nums, i, randomNum))
+        {
+            randomNum = rand() % range;
+            if (rand() % 2 == 1)
+                randomNum = -randomNum - 1;
+        }
+        printf("%d ", randomNum);
+        nums[i] = randomNum;
+        i++;
+    }
+    return 0;
+}
